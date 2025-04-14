2ae3010303564f5be6f504c08ee05fe409e6f479
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-04-14 11:01:08 +0200

    New components

---
+++ React_1/src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = (props) => {
+  return (
+    <div>
+      <h1>Name : {props.name}</h1>
+      <p>Price : {props.price}</p>
+    </div>
+  );
+};
+export default Product;
